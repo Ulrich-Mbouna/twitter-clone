134e96053767b97847f604d1e09a4052f775d7fa
Ulrich Mbouna <mbouna@kiroogames.com>
2022-06-21 14:30:49 +0100

    Create feed component and its header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 export default function Home() {
   return ( <
@@ -20,6 +21,7 @@ export default function Home() {
 
           {
             /* Feed */ }
+            <Feed />
 
         {
           /* Widgets */ }
