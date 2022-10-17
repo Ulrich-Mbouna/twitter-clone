b89fd3b058373689fbcccdb5fe03cd4daa9795b0
Mbouna Ulrich <60409730+Ulrich15@users.noreply.github.com>
2022-10-17 19:33:52 +0100

    Add Animation effect using framer-motion

--- components/Feed.js
+++ components/Feed.js
@@ -4,6 +4,7 @@ import Posts from "./Posts";
 import {useEffect, useState} from "react";
 import { onSnapshot, query, collection, orderBy } from "firebase/firestore"
 import {db} from "../firebase";
+import { AnimatePresence, motion} from "framer-motion";
 
 export default function Feed() {
   const [posts, setPosts] = useState([]);
@@ -20,9 +21,19 @@ export default function Feed() {
         </div>
       </div>
       <Input />
-      {posts.map((post) => (
-        <Posts key={post.id} post={post} />
-      ))}
+        <AnimatePresence>
+            {posts.map((post) => (
+                <motion.div
+                    key={post.id}
+                    initial={{opacity: 0}}
+                    animate={{opacity:1}}
+                    exit={{opacity:0}}
+                    transition={{ duration: 1}}
+                >
+                    <Posts key={post.id} post={post} />
+                </motion.div>
+            ))}
+        </AnimatePresence>
     </div>
   );
 }
--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,7 @@
 import { SearchIcon } from "@heroicons/react/outline";
 import { News } from './News'
 import {useState} from "react";
+import {AnimatePresence, motion} from "framer-motion";
 
 export default function Widgets({ newsResults, randomUsersResults }) {
     const [articleNumber, setArticleNumber] = useState(3);
@@ -20,25 +21,47 @@ export default function Widgets({ newsResults, randomUsersResults }) {
 
         <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
             <h4 className="font-bold text-xl px-4">What is happening</h4>
-            {newsResults.slice(0,articleNumber).map((article) => (
-                <News key={article.id} article={article} className="" />
-            ))}
+            <AnimatePresence>
+                {newsResults.slice(0,articleNumber).map((article) => (
+                    <motion.div
+                        key={article.title}
+                        initial={ { opacity: 0}}
+                        animate={ { opacity: 1 } }
+                        exit={{opacity: 0}}
+                        transition={ { duration: 1 }}
+                    >
+                        <News key={article.id} article={article} />
+                    </motion.div>
+                ))}
+
+            </AnimatePresence>
+
             <button onClick={() => setArticleNumber(articleNumber + 3)} className='text-blue-300 pl-4 pb-3 hover:text-blue-400'>Show more</button>
         </div>
         <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%] sticky top-16">
             <h4 className="font-bold text-xl px-4">Who to follow</h4>
-            {
-                randomUsersResults.slice(0,randomUserNumber).map((user, index) => (
-                    <div className='flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200' key={user.login.username}>
-                        <img className='rounded-full' width='40' src={user.picture.thumbnail} alt=""/>
-                        <div className="truncate ml-4 leading-5">
-                            <h4 className='font-bold hover:underline text-[14px]'>{user.login.username}</h4>
-                            <h5 className='text-[13px] text-gray-500 truncate'>{user.name.first + " " + user.name.last}</h5>
-                        </div>
-                        <button className='ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold'>Follow</button>
-                    </div>
-                ))
-            }
+            <AnimatePresence>
+                {
+                    randomUsersResults.slice(0,randomUserNumber).map((user, index) => (
+                        <motion.div
+                            key={user.login.username}
+                            initial={ { opacity: 0}}
+                            animate={ { opacity: 1 } }
+                            exit={{opacity: 0}}
+                            transition={ { duration: 1 }}
+                        >
+                            <div className='flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200 transition duration-500 ease-out' key={user.login.username}>
+                                <img className='rounded-full' width='40' src={user.picture.thumbnail} alt=""/>
+                                <div className="truncate ml-4 leading-5">
+                                    <h4 className='font-bold hover:underline text-[14px]'>{user.login.username}</h4>
+                                    <h5 className='text-[13px] text-gray-500 truncate'>{user.name.first + " " + user.name.last}</h5>
+                                </div>
+                                <button className='ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold'>Follow</button>
+                            </div>
+                        </motion.div>
+                    ))
+                }
+            </AnimatePresence>
             <button onClick={() => setRandomUserNumber(randomUserNumber + 3)} className='text-blue-300 pl-4 pb-3 hover:text-blue-400'>Show more</button>
         </div>
     </div>
